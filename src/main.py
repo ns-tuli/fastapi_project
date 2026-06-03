ea1f193e2ae960a71524e4d7d8a33a79161d9ffb
nstuli <nusratsiddiquetuli@gmail.com>
2026-06-04 02:03:24 +0600

    AI functions

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 import os
 from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+from .ai.gemini import Gemini
 
 app = FastAPI()
+
+def load_system_prompt():
+    try:
+        with open("src/prompts/system_prompt.md", "r") as f:
+            return f.read()
+    except FileNotFoundError:
+        return None
+    
+
+system_prompt = load_system_prompt()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY environment variable not set.")
+
+ai_platform = Gemini(api_key=gemini_api_key, system_prompt=system_prompt)
 
 
 
